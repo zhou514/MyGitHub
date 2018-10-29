77662cfb32c0d15078c786d7bc04b7288e1cb102
zhou514 <193900666@qq.com>
2018-10-29 19:26:17 +0800

    Update java/2018-10-29-2/Application.java

--- java/2018-10-29-2/Application.java
+++ java/2018-10-29-2/Application.java
@@ -1,7 +1,7 @@
 public class Application {
     public static void main(String args[]) {
         Simulator simulator = new Simulator();
-        Simulator.playSound(new Dog());
-        Simulator.playSound(new Cat());
+        simulator.playSound(new Dog());
+        simulator.playSound(new Cat());
     }
 }
